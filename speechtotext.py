626177728c8a67086bb05c7d8a81af28c26c143c
perilune42 <50865458+perilune42@users.noreply.github.com>
2024-05-31 18:09:43 -0700

    speech to lcd

--- speechtotext.py
+++ speechtotext.py
@@ -5,15 +5,52 @@
  
  
 import speech_recognition as sr
- 
+
+from subprocess import Popen, PIPE
+from time import sleep, perf_counter
+from datetime import datetime
+import board
+import digitalio
+import adafruit_character_lcd.character_lcd as characterlcd
+
+
 # Initialize the recognizer 
 r = sr.Recognizer() 
  
 
-     
-# Loop infinitely for user to
-# speak
+
+
+# Modify this if you have a different sized character LCD
+lcd_columns = 16
+lcd_rows = 2
+
+# compatible with all versions of RPI as of Jan. 2019
+# v1 - v3B+
+lcd_rs = digitalio.DigitalInOut(board.D22)
+lcd_en = digitalio.DigitalInOut(board.D17)
+lcd_d4 = digitalio.DigitalInOut(board.D25)
+lcd_d5 = digitalio.DigitalInOut(board.D24)
+lcd_d6 = digitalio.DigitalInOut(board.D23)
+lcd_d7 = digitalio.DigitalInOut(board.D18)
+
+
+# Initialise the lcd class
+lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6,
+                                    lcd_d7, lcd_columns, lcd_rows)
+
+# looking for an active Ethernet or WiFi device
+
+
+# wipe LCD screen before we start
+lcd.clear()
+
+
+
  
+# Loop infinitely for user to
+# speak 
+lcd.message = "Listening..."
+
 while(1):    
      
     # Exception handling to handle
@@ -38,10 +75,12 @@ while(1):
             
             MyText = MyText.lower()
  
-            print("Did you say ",MyText)
+            lcd.message = MyText
+            sleep(5)
+            lcd.message = "Listening..."
              
     except sr.RequestError as e:
-        print("Could not request results; {0}".format(e))
+        lcd.message = "Could not request results; {0}".format(e)
          
     except sr.UnknownValueError:
-        print("unknown error occurred")
+        lcd.message = "unknown error occurred"
